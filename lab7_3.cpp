48d82f11afb779865c6b9a5dafa29ad1852bf881
Namnaem <yositasatiman@gmail.com>
2023-12-12 13:32:55 +0700

    lab7_2.cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,18 +2,7 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
-}
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+    if(x < 'A' || x > 'Z') return '0';
+    if(x == 'A') return 'Z';
+    return x - 1;
 }
